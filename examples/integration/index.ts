50ce98854b306d83761d7a38bf78919035ac444e
Luiz Ferraz <luiz@lferraz.com>
2024-04-07 20:34:51 -0300

    Change example to show prototype Sitemap API

--- examples/integration/index.ts
+++ examples/integration/index.ts
@@ -1,4 +1,5 @@
 import type { AstroIntegration } from 'astro';
+import {getApi} from '@astrojs/sitemap';
 
 export default function createIntegration(): AstroIntegration {
 	// See the Integration API docs for full details
@@ -6,9 +7,23 @@ export default function createIntegration(): AstroIntegration {
 	return {
 		name: '@example/my-integration',
 		hooks: {
-			'astro:config:setup': () => {
+			'astro:config:setup': ({config}) => {
 				// See the @astrojs/react integration for an example
 				// https://github.com/withastro/astro/blob/main/packages/integrations/react/src/index.ts
+
+				const sitemapApi = getApi(config);
+				if (sitemapApi) {
+					sitemapApi.addPage(new URL('./foo', config.base).toString());
+
+					sitemapApi.addFilter(page => {
+						const pageUrl = new URL(page);
+
+						if (pageUrl.pathname.startsWith('/foo/hidden/')) {
+							// filter out our internal pages
+							return false;
+						}
+					})
+				}
 			},
 			'astro:build:setup': () => {
 				// See the @astrojs/lit integration for an example
